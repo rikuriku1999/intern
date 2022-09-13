ed29b17bec5e96fb0da8e4452dea447c4007a412
rikuriku1999 <kalashnikova1120@gmail.com>
2022-09-13 07:15:49 +0000

    requirements

---
+++ .vscode-server/data/User/History/74267776/FeK3.py
@@ -0,0 +1,40 @@
+import json
+import requests
+import pandas as pd
+# config.py
+import config
+import random
+
+api_key = config.HOTPEPPER_API_KEY
+
+i_start = 1
+restaurant_datas=[]
+
+user_data = [
+    'id':'id',
+    'area':'Z011',
+    'genre':'洋食',
+    ]
+
+
+def pick_restaurant(user_data):
+    rand_pick = random.randrange(1,30090)
+    print(rand_pick)
+    query = {
+        'key': api_key,
+        'large_area': user_data['Z011'], # 東京
+        'genre': user_data['genre'],
+        'order': 1, #名前の順
+        'start': rand_pick, #検索結果の何番目から出力するか
+        'count': 1, #最大取得件数
+        'format': 'json'
+    }
+    url_base = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+    responce = requests.get(url_base, query)
+    result = json.loads(responce.text)['results']['shop']
+    print(result)
+    for restaurant in result:
+        restaurant_datas.append([restaurant['name'], restaurant['address'], restaurant['budget']['name'], restaurant['genre']['name'], restaurant['urls']['pc']])
+    return restaurant_datas[0]
+
+print(pick_restaurant())
---
+++ .vscode-server/data/User/History/74267776/RtXW.py
@@ -0,0 +1,35 @@
+import json
+import requests
+import pandas as pd
+# config.py
+import config
+import random
+
+api_key = config.HOTPEPPER_API_KEY
+
+i_start = 1
+restaurant_datas=[]
+
+rand_pick = random.randrange(1,30090)
+
+print(rand_pick)
+
+query = {
+    'key': api_key,
+    'large_area': 'Z011', # 東京
+    'order': 1, #名前の順
+    'start': rand_pick, #検索結果の何番目から出力するか
+    'count': 1, #最大取得件数
+    'format': 'json'
+}
+url_base = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+responce = requests.get(url_base, query)
+result = json.loads(responce.text)['results']['shop']
+for restaurant in result:
+    restaurant_datas.append([restaurant['name'], restaurant['address'], restaurant['budget']['code'], restaurant['genre']['code']])
+
+print(restaurant_datas)
+
+#columns = ['name', 'address', 'budget', 'genre']
+#df_restaurants = pd.DataFrame(restaurant_datas, columns=columns)
+#df_restaurants.to_csv('restaurants_tokyo.csv')
